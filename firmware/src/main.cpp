0457fbb7705d30283aa6cdf9e6441bd7ced0e484
Adrià M. Cassorla <adria@adriamartinez.cat>
2024-06-13 12:38:49 +0200

    Updated simplified version

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,6 +1,4 @@
 #include <Arduino.h>
-
-#define FASTLED_FORCE_SOFTWARE_SPI
 #include <avdweb_Switch.h>
 #include <FastLED.h>
 
@@ -10,19 +8,17 @@
 #define MAX_AMPS 30000
 #define FRAMES_PER_SECOND 24
 
-#define NUM_LEDS_INFERIOR 120
-#define NUM_LEDS_LECTURA 288
+#define NUM_LEDS_AMBIENT 193
+#define NUM_LEDS_READING 288
 #define NUM_LEDS_TOP 107
-#define NUM_LEDS_TREN 73
+#define NUM_SWITCHES 6
 
-#define DATA_PIN_INFERIOR 1
-#define CLOCK_PIN_INFERIOR 0
-#define DATA_PIN_LECTURA 7
-#define CLOCK_PIN_LECTURA 6
-#define DATA_PIN_TOP 11
-#define CLOCK_PIN_TOP 10
-#define DATA_PIN_TREN 5
-#define CLOCK_PIN_TREN 4
+#define CLOCK_PIN_AMBIENT 13 // SCK
+#define DATA_PIN_AMBIENT 11 // MOSI
+#define CLOCK_PIN_TOP 27 // SCK1
+#define DATA_PIN_TOP 26 // MOSI1
+#define CLOCK_PIN_READING 49 // SCK2
+#define DATA_PIN_READING 50 // MOSI2
 
 #define POTEN_1 A1
 #define POTEN_2 A0
@@ -33,21 +29,19 @@
 #define SENSOR_R A14
 
 
-CRGB ledsInferior[NUM_LEDS_INFERIOR];
-CRGB ledsLectura[NUM_LEDS_LECTURA];
-CRGB ledsTop[NUM_LEDS_TOP];
-CRGB ledsTren[NUM_LEDS_TREN];
+CRGB ambientLeds[NUM_LEDS_AMBIENT];
+CRGB readingLeds[NUM_LEDS_READING];
+CRGB topLeds[NUM_LEDS_TOP];
 
-// Define the pin numbers for your switches
+// Define the pin numbers for the switches
 const uint8_t switchPins[] = { 25, 31, 27, 28, 29, 30 };
 const int switchPinsIds[] = { 0, 1, 2, 3, 4, 5 };
-const uint8_t numSwitches = 6;
 
 boolean isRainbowPlaying = false;
 boolean isTopOn = false;
 boolean isLeftOn = false;
 boolean isRightOn = false;
-boolean isTrenOn = false;
+boolean isChromotherapyOn = false;
 
 Switch switch0 = Switch(switchPins[0]);
 Switch switch1 = Switch(switchPins[1]);
@@ -56,25 +50,42 @@ Switch switch3 = Switch(switchPins[3]);
 Switch switch4 = Switch(switchPins[4]);
 Switch switch5 = Switch(switchPins[5]);
 
-Switch toggleSwitches[numSwitches] = { switch0, switch1, switch2, switch3, switch4, switch5 };
+Switch toggleSwitches[NUM_SWITCHES] = { switch0, switch1, switch2, switch3, switch4, switch5 };
 
-////// PALETTES /////
-
-// Gradient palette "bhw1_07_gp", originally from
-// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw1/tn/bhw1_07.png.index.html
+// Gradient palette "cw2_046_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/cw/2/tn/cw2-046.png.index.html
 // converted for FastLED with gammas (2.6, 2.2, 2.5)
-// Size: 8 bytes of program space.
+// Size: 12 bytes of program space.
 
-DEFINE_GRADIENT_PALETTE(trenPaletteSrc){
-  0, 229, 227, 1,
-  100, 255, 184, 83,
-  200, 255, 255, 125,
-  255, 255, 184, 83
+DEFINE_GRADIENT_PALETTE(darkGreenPalette){
+  0, 4, 12, 7,
+  127, 9, 40, 6,
+  255, 45, 67, 5
 };
-CRGBPalette16 trenPalette = trenPaletteSrc;
+
+// Gradient palette "Naturel_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/colo/smorin2002/tn/Naturel.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 40 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(lightGreenPalette){
+  0, 13, 62, 7,
+  51, 13, 62, 7,
+  51, 19, 68, 7,
+  102, 19, 68, 7,
+  102, 32, 77, 9,
+  153, 32, 77, 9,
+  153, 58, 79, 9,
+  204, 58, 79, 9,
+  204, 90, 80, 13,
+  255, 90, 80, 13
+};
+
+CRGBPalette16 bottomPalette = darkGreenPalette;
+CRGBPalette16 topPalette = lightGreenPalette;
 
 void toggleCallbackFunction(void *s) {
-  int *switchIndex = (int *)s;  // convert s to int pointer (int *)
+  int *switchIndex = (int *)s;  // converts s to int pointer (int *)
     switch ((*switchIndex)) {
     case 0:
       FastLED.clear();
@@ -84,32 +95,27 @@ void toggleCallbackFunction(void *s) {
     case 4:
       isTopOn = !isTopOn;
       if (isTopOn) {
-        fill_solid(ledsTop, NUM_LEDS_TOP, CRGB::Goldenrod);
+        fill_solid(topLeds, NUM_LEDS_TOP, CRGB::Goldenrod);
       } else {
-        fill_solid(ledsTop, NUM_LEDS_TOP, CRGB::Black);
-        if ((*switchIndex) == 1 && isTrenOn) {
-          isTrenOn = false;
-          fill_solid(ledsTren, NUM_LEDS_TREN, CRGB::Black);
-        }
+        fill_solid(topLeds, NUM_LEDS_TOP, CRGB::Black);
       }
       FastLED.show();
       break;
     case 5:
-      isTrenOn = !isTrenOn;
-      if (!isTrenOn) {
-        fill_solid(ledsTren, NUM_LEDS_TREN, CRGB::Black);
-        FastLED.show();
+      isChromotherapyOn = !isChromotherapyOn;
+       if (!isChromotherapyOn) {
+        FastLED.clear();
       }
       break;
     case 2:
       isLeftOn = !isLeftOn;
       if (isLeftOn) {
-        for (uint16_t i = NUM_LEDS_LECTURA - 1; i > NUM_LEDS_LECTURA / 2; --i) {
-          ledsLectura[i] = CRGB::Goldenrod;
+        for (uint16_t i = NUM_LEDS_READING - 1; i > NUM_LEDS_READING / 2; --i) {
+          readingLeds[i] = CRGB::Goldenrod;
         }
       } else {
-        for (uint16_t i = NUM_LEDS_LECTURA / 2; i < NUM_LEDS_LECTURA - 1; ++i) {
-          ledsLectura[i] = CRGB::Black;
+        for (uint16_t i = NUM_LEDS_READING / 2; i < NUM_LEDS_READING - 1; ++i) {
+          readingLeds[i] = CRGB::Black;
         }
       }
       FastLED.show();
@@ -117,14 +123,14 @@ void toggleCallbackFunction(void *s) {
     case 3:
       isRightOn = !isRightOn;
       if (isRightOn) {
-        for (uint8_t i = 0; i < NUM_LEDS_LECTURA / 2; ++i) {
-          ledsLectura[i] = CRGB::Goldenrod;
+        for (uint8_t i = 0; i < NUM_LEDS_READING / 2; ++i) {
+          readingLeds[i] = CRGB::Goldenrod;
         }
       } else {
-        for (uint8_t i = NUM_LEDS_LECTURA / 2; i > 0; --i) {
-          ledsLectura[i] = CRGB::Black;
+        for (uint8_t i = NUM_LEDS_READING / 2; i > 0; --i) {
+          readingLeds[i] = CRGB::Black;
         }
-        ledsLectura[0] = CRGB::Black;
+        readingLeds[0] = CRGB::Black;
       }
       FastLED.show();
       break;
@@ -137,10 +143,9 @@ void setup() {
   // sanity check delay - allows reprogramming if accidently blowing power w/leds
   delay(2000);
 
-  FastLED.addLeds<CHPSET, DATA_PIN_INFERIOR, CLOCK_PIN_INFERIOR, COLOR_ORDER>(ledsInferior, NUM_LEDS_INFERIOR);
-  FastLED.addLeds<CHPSET, DATA_PIN_LECTURA, CLOCK_PIN_LECTURA, COLOR_ORDER>(ledsLectura, NUM_LEDS_LECTURA);
-  FastLED.addLeds<CHPSET, DATA_PIN_TOP, CLOCK_PIN_TOP, COLOR_ORDER>(ledsTop, NUM_LEDS_TOP);
-  FastLED.addLeds<CHPSET, DATA_PIN_TREN, CLOCK_PIN_TREN, COLOR_ORDER>(ledsTren, NUM_LEDS_TREN);
+  FastLED.addLeds<CHPSET, DATA_PIN_AMBIENT, CLOCK_PIN_AMBIENT, COLOR_ORDER>(ambientLeds, NUM_LEDS_AMBIENT);
+  FastLED.addLeds<CHPSET, DATA_PIN_READING, CLOCK_PIN_READING, COLOR_ORDER>(readingLeds, NUM_LEDS_READING);
+  FastLED.addLeds<CHPSET, DATA_PIN_TOP, CLOCK_PIN_TOP, COLOR_ORDER>(topLeds, NUM_LEDS_TOP);
 
 
   FastLED.setMaxPowerInVoltsAndMilliamps(VOLTS, MAX_AMPS);
@@ -148,19 +153,44 @@ void setup() {
   FastLED.show();
 
 
-  for (uint8_t i = 0; i < numSwitches; ++i) {
+  for (uint8_t i = 0; i < NUM_SWITCHES; ++i) {
     toggleSwitches[i].setPushedCallback(&toggleCallbackFunction, (void *)&switchPinsIds[i]);
     toggleSwitches[i].setReleasedCallback(&toggleCallbackFunction, (void *)&switchPinsIds[i]);
   }
 }
 
+void loop() {
+  EVERY_N_MILLISECONDS(50) {
+    for (uint8_t i = 0; i < NUM_SWITCHES; ++i) {
+      toggleSwitches[i].poll();
+    }
+  }
+
+  EVERY_N_MILLISECONDS(1000 / FRAMES_PER_SECOND) {
+    if (isRainbowPlaying) {
+      rainbow_beat();
+    }
+    if (isChromotherapyOn) {
+      uint8_t brightness = map(analogRead(POTEN_3), 0, 1023, 10, 255);
+      chromotherapy(brightness);
+    }
+    if (isRainbowPlaying || isChromotherapyOn) {
+      FastLED.show();
+    }
+  }
+}
+
+//////////////
+// Helprers
+//////////////
+
 void rainbow_beat() {
   // Starting hue
   uint8_t beatA = beatsin8(9, 0, 255);
   uint8_t beatB = beatsin8(13, 0, 255);
-  fill_rainbow(ledsInferior, NUM_LEDS_INFERIOR, (beatA + beatB) / 2, 255 / NUM_LEDS_INFERIOR);
-  fill_rainbow(ledsLectura, NUM_LEDS_LECTURA, (beatA + beatB) / 2, NUM_LEDS_LECTURA / 255);
-  fill_rainbow(ledsTop, NUM_LEDS_TOP, (beatA + beatB) / 2, 255 / NUM_LEDS_TOP);
+  fill_rainbow(ambientLeds, NUM_LEDS_AMBIENT, (beatA + beatB) / 2, 255 / NUM_LEDS_AMBIENT);
+  fill_rainbow(readingLeds, NUM_LEDS_READING, (beatA + beatB) / 2, NUM_LEDS_READING / 255);
+  fill_rainbow(topLeds, NUM_LEDS_TOP, (beatA + beatB) / 2, 255 / NUM_LEDS_TOP);
 }
 
 void applyRandomPalette(struct CRGB *targetArray, CRGBPalette16 &pal, int numLeds, int indexScale, int minBrightness, int maxBrightness) {
@@ -175,26 +205,8 @@ void applyRandomPalette(struct CRGB *targetArray, CRGBPalette16 &pal, int numLed
   }
 }
 
-void maquetaSuau() {
-  applyRandomPalette(ledsTren, trenPalette, NUM_LEDS_TREN, 100, 50, 80);
-}
-
-void loop() {
-  EVERY_N_MILLISECONDS(50) {
-    for (uint8_t i = 0; i < numSwitches; ++i) {
-      toggleSwitches[i].poll();
-    }
-  }
-
-  EVERY_N_MILLISECONDS(1000 / FRAMES_PER_SECOND) {
-    if (isRainbowPlaying) {
-      rainbow_beat();
-    }
-    if (isTrenOn) {
-      maquetaSuau();
-    }
-    if (isTrenOn || isRainbowPlaying) {
-      FastLED.show();
-    }
-  }
+void chromotherapy(int maxBrightness) {
+  applyRandomPalette(ambientLeds, bottomPalette, NUM_LEDS_AMBIENT, 75, maxBrightness * 0.2, maxBrightness);
+  applyRandomPalette(readingLeds, topPalette, NUM_LEDS_READING, 10, maxBrightness * 0.2, maxBrightness * 0.7);
+  applyRandomPalette(topLeds, topPalette, NUM_LEDS_TOP, 100, maxBrightness * 0.2, maxBrightness);
 }
