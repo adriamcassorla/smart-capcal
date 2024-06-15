908106ff13347bd344eb0e07b923c74883ee28a1
Adrià M. Cassorla <adria@adriamartinez.cat>
2024-06-15 11:50:30 +0200

    Splitted switches

--- firmware/include/switches.h
+++ firmware/include/switches.h
@@ -1,13 +1,25 @@
 #ifndef SWITCHES_H
 #define SWITCHES_H
 
+#include <avdweb_Switch.h>
+#include <Arduino.h>
+
+#define NUM_SWITCHES 6
+
+extern uint8_t switchPins[NUM_SWITCHES];
+extern int switchPinsIds[NUM_SWITCHES];
+extern class Switches switches;
 class Switches {
 public:
-    Switches(); 
-    void init() const;
-    void poll() const; 
+  Switches(uint8_t *p, int *ids);
+  void setup();
+  void poll();
+
 private:
-    
+  uint8_t *pins;
+  int *pinIds;
+  Switch *toggleSwitches[NUM_SWITCHES];
+  static void callback(void *s);
 };
 
 #endif // SWITCHES_H
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,13 +2,11 @@
 
 #include <Arduino.h>
 #include <FastLED.h>
-#include <avdweb_Switch.h>
 #include "lights.h"
+#include "switches.h"
 
 #define FRAMES_PER_SECOND 24
 #define INITIAL_DELAY 2000
-
-#define NUM_SWITCHES 6
 
 #define POTEN_1 A1
 #define POTEN_2 A0
@@ -18,76 +16,18 @@
 #define SENSOR_L A15
 #define SENSOR_R A14
 
-// Define the pin numbers for the switches
-const uint8_t switchPins[] = { 25, 31, 27, 28, 29, 30 };
-const int switchPinsIds[] = { 0, 1, 2, 3, 4, 5 };
-
-Switch switch0 = Switch(switchPins[0]);
-Switch switch1 = Switch(switchPins[1]);
-Switch switch2 = Switch(switchPins[2]);
-Switch switch3 = Switch(switchPins[3]);
-Switch switch4 = Switch(switchPins[4]);
-Switch switch5 = Switch(switchPins[5]);
-
-Switch toggleSwitches[NUM_SWITCHES] = { switch0, switch1, switch2, switch3, switch4, switch5 };
-
-void toggleCallbackFunction(void *s) {
-  int *switchIndex = (int *)s;  // converts s to int pointer (int *)
-
-  // Turns off animations and resets all other lights
-  if (demoLights.isOn && (*switchIndex) != 0 && (*switchIndex) != 5)
-  {
-    demoLights.stop();
-  }
-  else if ((*switchIndex) == 0 || (*switchIndex) == 5)
-  {
-    ambientLight.reset();
-    readingLeft.reset();
-    readingRight.reset();
-  }
-
-    switch ((*switchIndex)) {
-    case 0:
-      demoLights.setMode(DemoLights::Mode::Rainbow);
-      demoLights.toggle();
-      break;
-    case 1:
-    case 4:
-      ambientLight.toggle();
-      break;
-    case 5:
-      demoLights.setMode(DemoLights::Mode::Chromotherapy);
-      demoLights.toggle();
-      break;
-    case 2:
-      readingLeft.toggle();
-      break;
-    case 3:
-      readingRight.toggle();
-      break;
-    default:
-      break;
-  }
-}
-
 void setup() {
   // sanity check delay - allows reprogramming if accidently blowing power w/leds
   delay(INITIAL_DELAY);
 
   // Setup internal parts
   setupLights();
-
-  for (uint8_t i = 0; i < NUM_SWITCHES; ++i) {
-    toggleSwitches[i].setPushedCallback(&toggleCallbackFunction, (void *)&switchPinsIds[i]);
-    toggleSwitches[i].setReleasedCallback(&toggleCallbackFunction, (void *)&switchPinsIds[i]);
-  }
+  switches.setup();
 }
 
 void loop() {
   EVERY_N_MILLISECONDS(50) {
-    for (uint8_t i = 0; i < NUM_SWITCHES; ++i) {
-      toggleSwitches[i].poll();
-    }
+    switches.poll();
     uint8_t brightness = map(analogRead(POTEN_3), 0, 1023, 10, MAX_BRIGHTNESS);
     demoLights.setBrightness(brightness);
   }
